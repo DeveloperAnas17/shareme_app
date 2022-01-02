7254f778b7bea94d3753f50499edc8a3bdcb4d5c
Anas <amanabbasi3660@gmail.com>
2022-01-02 16:09:45 +0530

    add header

--- sanity-frontend/src/components/Login.js
+++ sanity-frontend/src/components/Login.js
@@ -5,7 +5,11 @@ import { FcGoogle } from "react-icons/fc";
 import shareVideo from "../assets/share.mp4";
 import logo from "../assets/logowhite.png";
 
+import { client } from "../client";
+
 const Login = () => {
+  const navigate = useNavigate();
+
   const responseGoogle = (response) => {
     console.log(response);
     localStorage.setItem("user", JSON.stringify(response.profileObj));
@@ -18,6 +22,10 @@ const Login = () => {
       userName: name,
       image: imageUrl,
     };
+
+    client.createIfNotExists(doc).then(() => {
+      navigate("/", { replace: true });
+    });
   };
 
   return (
---
+++ sanity-frontend/src/containers/Pins.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Pins = () => {
+  return (
+    <div>
+      <h1>Pins</h1>
+    </div>
+  );
+};
+
+export default Pins;
